74fbfa6e50f767a48dbdbee207e3d9c20aee58b0
Sergey Danilov <sergeydanilov@gmail.com>
2018-05-24 20:52:28 +0800

    added oval

--- src/main/java/me/dz2/teaching/masterclazz/Main.java
+++ src/main/java/me/dz2/teaching/masterclazz/Main.java
@@ -1,9 +1,43 @@
 package me.dz2.teaching.masterclazz;
 
+import com.sun.xml.internal.bind.v2.model.annotation.RuntimeAnnotationReader;
+
+import javax.swing.*;
+
 public class Main {
+
+    MyPanel panel;
+
+    class Background implements Runnable {
+
+        int i = 0;
+
+        @Override
+        public void run() {
+            while (i< 1000) {
+//                panel.set
+                i++;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("hi");
+
+        JFrame window = new JFrame();
+
+        window.setSize(1024, 768);
+        window.setLocation(0,0);
+        window.setVisible(true);
+
+        window.setTitle("Hello world");
+
+        MyPanel panel = new MyPanel();
+        window.add(panel);
+
+
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 }
---
+++ src/main/java/me/dz2/teaching/masterclazz/MyPanel.java
@@ -0,0 +1,33 @@
+package me.dz2.teaching.masterclazz;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ */
+public class MyPanel extends JPanel {
+
+    int d = 30;
+    Color bgColor = new Color(0,0,255);
+    Color cicleColor = new Color(255,0,0);
+
+    int x =0;
+    int y = 100;
+
+    int speed = 1;
+
+
+    @Override
+    protected void paintComponent(Graphics canvas) {
+        super.paintComponent(canvas);
+
+//        canvas.fillRect(100,50, 400,200);
+        canvas.setColor(bgColor);
+        canvas.fillRect(0, 0, getWidth(), getHeight());
+
+        canvas.setColor(cicleColor);
+        canvas.fillOval(x, y, d, d);
+
+    }
+}
